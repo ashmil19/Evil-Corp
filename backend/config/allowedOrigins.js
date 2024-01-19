746ce4056f9c3af07e1278b4053c3fe0c7853fe1
Ashmil Abdul Rasheed <ashmilabdulrasheed@gmail.com>
2024-01-19 08:52:07 +0530

    origin urls added

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://www.evilcorp.ashmil.shop",
   "wss://www.evilcorp.ashmil.shop",
-  "https://evil-corp123.vercel.app"
+  "https://evil-corp123.vercel.app",
+  "https://evilcorp123.netlify.app/"
 ]
 
 module.exports = {
